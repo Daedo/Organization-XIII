0f0e3c1e26a4bebabda3fe453f7d2b7ec84ecea2
Daedo <public@dominik-spies.de>
2020-06-10 07:40:24 +0200

    Deploy Demo Page

--- src/app/model/generator-options.ts
+++
@@ -1,9 +0,0 @@
-import { LanguageModelOptions } from './language-model-options';
-
-export class GeneratorOptions {
-	// tslint:disable: align
-	constructor(public readonly name: string, public readonly wordCount: number,
-				public readonly numberOfNames: number, public readonly beamSize: number,
-				public readonly sigil: string, public readonly modelOptions: LanguageModelOptions) {
-	}
-}
--- src/app/model/language-model-options.ts
+++
@@ -1,6 +0,0 @@
-export class LanguageModelOptions {
-	// tslint:disable: align
-	constructor(public readonly biWeight: number, public readonly triWeight: number,
-				public readonly quadWeight: number, public readonly nGramBackoff: boolean) {
-	}
-}
